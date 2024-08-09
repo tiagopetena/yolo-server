f9384d1b7707fabf3c4da0667b64655166b99762
Tiago Petena <tiagopetena@gmail.com>
2024-08-09 11:25:19 -0300

    finished thing

--- client/client.py
+++ client/client.py
@@ -10,31 +10,38 @@ VIDEO_PATH = os.getenv('VIDEO_PATH')
 
 
 async def send_frames():
-    async with websockets.connect("ws://localhost:8000/video") as websocket:
-        video_path = str(VIDEO_PATH)
-        cap = cv2.VideoCapture(video_path)
-        frame_n = -1
-        people_count = 0
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame_n += 1
-            if frame_n <= 360:
-                continue
-            # Convert frame to bytes
-            _, encoded = cv2.imencode(".jpg", frame)
-            await websocket.send(encoded.tobytes())
+    while True:
+        try:
+            async with websockets.connect("ws://0.0.0.0:8000/video") as websocket:
+                video_path = str(VIDEO_PATH)
+                cap = cv2.VideoCapture(video_path)
+                frame_n = -1
+                people_count = 0
+                while cap.isOpened():
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    frame_n += 1
+                    if frame_n <= 360:
+                        continue
+                    # Convert frame to bytes
+                    _, encoded = cv2.imencode(".jpg", frame)
+                    await websocket.send(encoded.tobytes())
 
-            ack = await websocket.recv()
+                    ack = await websocket.recv()
 
-            detections = json.loads(str(ack))
+                    detections = json.loads(str(ack))
 
-            for i, bbox in enumerate(detections):
-                if bbox["id"] >= people_count:
-                    people_count = bbox["id"]
+                    for i, bbox in enumerate(detections):
+                        if bbox["id"] >= people_count:
+                            people_count = bbox["id"]
 
-        print(people_count)
+                    with open("./peple.txt", "a") as f:
+                        f.write(f"{frame_n}: {people_count}")
+
+                print(people_count, flush=True)
+        except Exception:
+            pass
 
 
 asyncio.run(send_frames())
